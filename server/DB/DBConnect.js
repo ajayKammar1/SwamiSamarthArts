aaf7096b87195a741a7616c99423bdc38afb7855
Ajay Kammar <kammarajay1@gmail.com>
2024-07-03 01:33:04 -0700

    DB Atlas Link is Added

--- server/DB/DBConnect.js
+++ server/DB/DBConnect.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/SamarthArts")
+  .connect(
+    "mongodb+srv://kammarajay1:ajay1kammar@cluster0.kqko83l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => console.log("DB is Connected"))
   .catch((err) => console.log(err));
